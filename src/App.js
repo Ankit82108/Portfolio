fab257860f95d3e33ecd04375bc0a25ed60b31ea
raviaws523 <raviaws523@gmail.com>
2025-09-30 18:00:46 +0530

    change

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import Header from './components/Header/Header';
 import Hero from './components/Hero/Hero';
 import Works from './components/Works/Works';
 import About from './components/About/About';
-
-// React Router
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Intro from './components/About/RouteComponents/Intro';
 import Experience from './components/About/RouteComponents/Experience';
@@ -18,28 +16,31 @@ import Contact from './components/Contact/Contact';
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route
-            exact
-            path="/"
-            element={
-              <>
-                <Header /> <Hero /> <Works /> <About /> <Skill /> <Contact />
-                <Footer />
-              </>
-            }
-          />
-          <Route exact path="/about/intro" element={<Intro />} />
-          <Route exact path="/about/experience" element={<Experience />} />
-          <Route exact path="/about/internships" element={<Internships />} />
-          <Route exact path="/about/graduation" element={<Graduation />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter basename="/Portfolio">
+      <Routes>
+        <Route
+          exact
+          path="/"
+          element={
+            <>
+              <Header />
+              <Hero />
+              <Works />
+              <About />
+              <Skill />
+              <Contact />
+              <Footer />
+            </>
+          }
+        />
+        <Route exact path="/about/intro" element={<Intro />} />
+        <Route exact path="/about/experience" element={<Experience />} />
+        <Route exact path="/about/internships" element={<Internships />} />
+        <Route exact path="/about/graduation" element={<Graduation />} />
+        {/* <Route exact path="/about/graduation" element={<Graduation />} /> */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
-
